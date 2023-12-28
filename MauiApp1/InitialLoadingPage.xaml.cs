cc8695b4d12136f4c89a4616f0d8c442cbecb018
Iancu Andrei <79975904+andrei0200@users.noreply.github.com>
2023-12-28 14:43:05 +0200

    new page

--- MauiApp1/InitialLoadingPage.xaml.cs
+++ MauiApp1/InitialLoadingPage.xaml.cs
@@ -1,9 +1,45 @@
+using System.Net.NetworkInformation;
+
 namespace MauiApp1;
 
 public partial class InitialLoadingPage : ContentPage
 {
+	string isConnected = "Fara conexiune";
 	public InitialLoadingPage()
 	{
 		InitializeComponent();
+		if (CheckInternetConnection())
+            {
+				isConnected = "Cu internet";
+            }
+            else
+            {
+                
+            }
+		this.BindingContext = this;
+	}
+
+	public string IsConnected // Property to bind to the XAML
+        {
+            get { return isConnected; }
+            set
+            {
+                isConnected = value;
+                OnPropertyChanged(nameof(IsConnected)); // Notify XAML of property change
+            }
+        }
+		
+	public static bool CheckInternetConnection()
+	{
+		try
+		{
+			Ping ping = new Ping();
+			PingReply reply = ping.Send("www.google.com");
+			return reply.Status == IPStatus.Success;
+		}
+		catch (PingException)
+		{
+			return false;
+		}
 	}
 }
