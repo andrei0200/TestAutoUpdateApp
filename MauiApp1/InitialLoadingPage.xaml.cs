61eef87b637a9b727bb1fec82c8633b44bcc6685
Iancu Andrei <79975904+andrei0200@users.noreply.github.com>
2023-12-28 14:37:42 +0200

    Internet connection test

---
+++ MauiApp1/InitialLoadingPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1;
+
+public partial class InitialLoadingPage : ContentPage
+{
+	public InitialLoadingPage()
+	{
+		InitializeComponent();
+	}
+}
--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,13 +1,49 @@
-﻿namespace MauiApp1;
+﻿using System.Net.NetworkInformation;
+
+namespace MauiApp1;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+	string isConnected = "Fara conexiune";
 
 	public MainPage()
 	{
 		InitializeComponent();
+		if (CheckInternetConnection())
+            {
+				isConnected = "Cu internet";
+            }
+            else
+            {
+                
+            }
+		this.BindingContext = this;
 	}
+
+	public static bool CheckInternetConnection()
+	{
+		try
+		{
+			Ping ping = new Ping();
+			PingReply reply = ping.Send("www.google.com");
+			return reply.Status == IPStatus.Success;
+		}
+		catch (PingException)
+		{
+			return false;
+		}
+	}
+
+	public string IsConnected // Property to bind to the XAML
+        {
+            get { return isConnected; }
+            set
+            {
+                isConnected = value;
+                OnPropertyChanged(nameof(IsConnected)); // Notify XAML of property change
+            }
+        }
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
